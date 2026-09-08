2fd6f391b16df4093b75479677ea75d299550a17
Bafana Shangase <102722321+bafana007@users.noreply.github.com>
2026-09-08 13:14:57 +0200

    Delete inventory-web (1)/inventory-web/inventory-web directory

--- inventory-web (1)/inventory-web/inventory-web/js/views/landing.js
+++
@@ -1,9 +0,0 @@
-window.Views = window.Views || {};
-window.Views.landing = (function () {
-  async function landing() {
-    const html = await Templates.load("landing");
-    return html.replace("<div data-insert=\"flashes\"></div>", UI.flashesHtml());
-  }
-
-  return { landing: landing };
-})();
